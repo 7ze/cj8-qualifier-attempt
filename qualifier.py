879b772c9d7bc26429e3cfc81d2b95d55964986b
The_philosopher <shinudonney@tuta.io>
2021-06-27 20:59:57 +0530

    Add basic logic

--- qualifier.py
+++ qualifier.py
@@ -4,12 +4,17 @@ from typing import Any, List, Optional
 def make_table(
     rows: List[List[Any]], labels: Optional[List[Any]] = None, centered: bool = False
 ) -> str:
-    table = rows
+    table = ""
+    for row in rows:
+        table_row = " ".join(map(str, row))
+        table = f"{table}\n{table_row}"
     return table
 
 
 def display_table(table):
-    print(f"table:\n{table}")
+    print("\n***** TABLE: *****")
+    print(table)
+    print("\n")
 
 
 table = make_table(
